6069d04cbff358789a528eb1977deaf601f4bcf7
skinnynoonie <skinnynoonie@users.noreply.github.com>
2024-01-07 10:48:51 -0500

    Static factory methods for JsonFormConverter

--- src/main/java/me/skinnynoonie/noonieconfigs/converter/JsonFormConverter.java
+++ src/main/java/me/skinnynoonie/noonieconfigs/converter/JsonFormConverter.java
@@ -2,16 +2,36 @@ package me.skinnynoonie.noonieconfigs.converter;
 
 import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import me.skinnynoonie.noonieconfigs.exception.MalformedBodyException;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Type;
+import java.util.Map;
+
 public final class JsonFormConverter implements RawFormConverter<JsonObject> {
+
+    public static JsonFormConverter createDefault() {
+        return new JsonFormConverter(new GsonBuilder().serializeNulls().create());
+    }
+
+    public static JsonFormConverter withTypeAdapters(@NotNull Map<@NotNull Type, @NotNull Object> typeAdapters) {
+        Preconditions.checkNotNull(typeAdapters, "Parameter typeAdapters is null.");
+
+        GsonBuilder gsonBuilder = new GsonBuilder().serializeNulls();
+        typeAdapters.forEach(gsonBuilder::registerTypeAdapter);
+        return new JsonFormConverter(gsonBuilder.create());
+    }
+
+    public static JsonFormConverter newInstance(@NotNull Gson gson) {
+        return new JsonFormConverter(gson);
+    }
 
     private final Gson gson;
 
-    public JsonFormConverter(@NotNull Gson gson) {
+    private JsonFormConverter(@NotNull Gson gson) {
         Preconditions.checkNotNull(gson, "Parameter gson is null.");
 
         this.gson = gson;
